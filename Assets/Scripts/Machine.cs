2d64fae632f5c894fb3442f275859e2c6a3af52d
DESKTOP-L6CKTNP\Abdullah <abdullahshosha197@gmail.com>
2022-02-19 02:01:14 +0200

    making One Local Script Machine(More OOP)

--- Assets/Scripts/Machine.cs
+++ Assets/Scripts/Machine.cs
@@ -7,33 +7,24 @@ public class Machine : MonoBehaviour
 {
 
     private float time = 0;
-    public static string WorkingName, WorkingViewName;
+    public string WorkingName, WorkingViewName;
     protected Collider ambool;
-    public Text Ttext;
     public static bool Vortexon;
+    public Text Timee, TriggerStayTime;
+    public Animator Open;
 
-    
+
     private void OnTriggerStay(Collider other)
     {
-        if (gameObject.GetComponent<CenterControl>())
-        {
-            WorkingName = "Center";
-            WorkingViewName = "ViewCenter";
-        }
-        else if (gameObject.GetComponent<ThermoControl>())
-        {
-            WorkingName = "Thermo";
-            WorkingViewName = "ViewThermo";
-        }
 
         if (time >= 2.0f)
         {
-            Ttext.text = ((int)time).ToString();
+            TriggerStayTime.text = ((int)time).ToString();
             if (other.tag == "Centerfugtunes" && 
                 other.GetComponent<CenterfugtunesControl>().Inside.Count > 0)
             {
                 ambool = other;
-                ChangeView();
+                ChangeCameraView(WorkingViewName,"Base", WorkingName);
             }
             
         }
@@ -43,29 +34,60 @@ public class Machine : MonoBehaviour
 
 
 
-    public void ChangeView()
+    //Camera Animation
+    public void ChangeCameraView(string Dissable1, string Dissable2, string Enable)
     {
-        GameObject.Find("mainCamera").GetComponent<Animator>().SetBool(WorkingViewName, false);
-        GameObject.Find("mainCamera").GetComponent<Animator>().SetBool("Base", false);
-        GameObject.Find("mainCamera").GetComponent<Animator>().SetBool(WorkingName, true);
+        GameObject.Find("mainCamera").GetComponent<Animator>().SetBool(Dissable1, false);
+        GameObject.Find("mainCamera").GetComponent<Animator>().SetBool(Enable, false);
+        GameObject.Find("mainCamera").GetComponent<Animator>().SetBool(Dissable2, true);
     }
-    public void StartWorking()
-    {
-        GameObject.Find("mainCamera").GetComponent<Animator>().SetBool("Base", false);
-        GameObject.Find("mainCamera").GetComponent<Animator>().SetBool(WorkingName, false);
-        GameObject.Find("mainCamera").GetComponent<Animator>().SetBool(WorkingViewName, true);
-    }
-    public void Reset()
-    {
-        GameObject.Find("mainCamera").GetComponent<Animator>().SetBool(WorkingName, false);
-        GameObject.Find("mainCamera").GetComponent<Animator>().SetBool(WorkingViewName, false);
-        GameObject.Find("mainCamera").GetComponent<Animator>().SetBool("Base", true);
-    }
+    
+
+
     public void Votrex()
     {
         Vortexon = !Vortexon;
         gameObject.GetComponent<Animator>().SetBool("IsWorking", Vortexon);
     }
+
+
+
+
+    public void StartCorontine()
+    {
+        ChangeCameraView("base", WorkingName, WorkingViewName);
+        TurnOn();
+        StartCoroutine(SatrtWorking());
+
+    }
+
+
+    public IEnumerator SatrtWorking()
+    {
+        while (ChangeValue.MVal > 0)
+        {
+            yield return new WaitForSeconds(1);
+            ChangeValue.MVal--;
+            Timee.text = ChangeValue.MVal.ToString();
+        }
+        ChangeCameraView(WorkingName, WorkingViewName, "Base");
+        ChangeValue.RVal = 0;
+        //Temp.text = ChangeValue.RVal.ToString();
+        TurnOff();
+        ambool.GetComponent<CenterfugtunesControl>().ThermoDone = true;
+    }
+
+
+    public void TurnOff()
+    {
+        Open.SetBool("IsWorking", false);
+    }
+    public void TurnOn()
+    {
+        Open.SetBool("IsWorking", true);
+    }
+
+
     private void OnTriggerExit(Collider other)
     {
         time = 0;
